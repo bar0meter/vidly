098108f42050264e53854012a04414c35486f2b3
barometer060 <ajinkya.a.gaikwad@capgemini.com>
2018-08-27 15:16:59 +0530

    restructured the app

--- index.js
+++ index.js
@@ -1,65 +1,31 @@
-const Joi = require("joi");
 const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
 const app = express();
+const config = require("config");
+const genres = require("./routes/genres");
+const home = require("./routes/home");
+const logger = require("./middleware/logger");
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
 app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(helmet());
+app.use("/api/genres", genres);
+app.use("/", home);
 
-const genres = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Horror" },
-  { id: 3, name: "Romance" }
-];
-
-app.get("/api/genres", (req, res) => {
-  res.send(genres);
-});
-
-app.get("/api/genres/:id", (req, res) => {
-  const genre = genres.find(c => c.id === parseInt(req.params.id));
-
-  if (!genre) return res.status(404).send("Genre with given Id not found");
-  res.send(genre);
-});
-
-app.post("/api/genres", (req, res) => {
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name
-  };
-  genres.push(genre);
-  res.send(genre);
-});
-
-app.put("/api/genres/:id", (req, res) => {
-  const genre = genres.find(c => c.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send("Genre with given Id not found");
-
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  genre.name = req.body.name;
-  res.send(genre);
-});
-
-app.delete("/api/genres/:id", (req, res) => {
-  const genre = genres.find(c => c.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send("Genre with given Id not found");
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-  res.send(genre);
-});
-
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
-  return Joi.validate(genre, schema);
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan enabled....");
 }
+
+console.log("Application Name : " + config.get("name"));
+console.log("Mail Server : " + config.get("mail.host"));
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
