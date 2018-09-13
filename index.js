58ff3dccc1f701e463db3f7d3946b42c15d96d9c
frost060 <ajinkya060@gmail.com>
2018-09-13 18:28:46 +0530

    Added Authorization Middleware

--- index.js
+++ index.js
@@ -1,52 +1,32 @@
-const express = require("express");
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
-const helmet = require("helmet");
-const morgan = require("morgan");
+const config = require('config');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const mongoose = require('mongoose');
+const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+const express = require('express');
 const app = express();
-const config = require("config");
-const genres = require("./routes/genres");
-const home = require("./routes/home");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const logger = require("./middleware/logger");
-const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb://localhost/vidly")
-  .then(() => console.log("Connected to the database"))
-  .catch(err => console.error("Error connecting to the database...", err));
-
-app.set("view engine", "pug");
-app.set("views", "./views");
-
-app.use(express.json());
-app.use(express.static("public"));
-app.use(
-  express.urlencoded({
-    extended: true
-  })
-);
-app.use(helmet());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-app.use("/", home);
-
-if (app.get("env") === "development") {
-  app.use(morgan("tiny"));
-  console.log("Morgan enabled....");
+if (!config.get('jwtPrivateKey')) {
+  console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+  process.exit(1);
 }
 
-console.log("Application Name : " + config.get("name"));
-console.log("Mail Server : " + config.get("mail.host"));
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
